1ca74494c6658c08c1af3802159d3e4ccee2bef4
Dmitry <cher-di@mail.ru>
2020-06-19 01:41:41 +0500

    Fixed bugs

--- main.py
+++ main.py
@@ -31,29 +31,29 @@ def find_intersection_dots(a: float, b: float, c: float, d: float) -> _Tuple[_Tu
     result = []
 
     if d1 >= 0:
-        x1 = (-(b - 1) + d1 ** 0.5) // (2 * a)
-        x2 = (-(b - 1) - d1 ** 0.5) // (2 * a)
+        x1 = (-(b - 1) + d1 ** 0.5) / (2 * a)
+        x2 = (-(b - 1) - d1 ** 0.5) / (2 * a)
         for x in (x1, x2):
             if x >= 1 and abs(x - 1) - d >= 0:
                 result.append((x, _y(x)))
 
     if d2 >= 0:
-        x1 = (-(b + 1) + d2 ** 0.5) // (2 * a)
-        x2 = (-(b + 1) - d2 ** 0.5) // (2 * a)
+        x1 = (-(b + 1) + d2 ** 0.5) / (2 * a)
+        x2 = (-(b + 1) - d2 ** 0.5) / (2 * a)
         for x in (x1, x2):
             if x < 1 and abs(x - 1) - d >= 0:
                 result.append((x, _y(x)))
 
     if d3 >= 0:
-        x1 = (-(b + 1) + d3 ** 0.5) // (2 * a)
-        x2 = (-(b + 1) - d3 ** 0.5) // (2 * a)
+        x1 = (-(b + 1) + d3 ** 0.5) / (2 * a)
+        x2 = (-(b + 1) - d3 ** 0.5) / (2 * a)
         for x in (x1, x2):
             if x >= 1 and abs(x - 1) - d < 0:
                 result.append((x, _y(x)))
 
     if d4 >= 0:
-        x1 = (-(b - 1) + d4 ** 0.5) // (2 * a)
-        x2 = (-(b - 1) - d4 ** 0.5) // (2 * a)
+        x1 = (-(b - 1) + d4 ** 0.5) / (2 * a)
+        x2 = (-(b - 1) - d4 ** 0.5) / (2 * a)
         for x in (x1, x2):
             if x < 1 and abs(x - 1) - d < 0:
                 result.append((x, _y(x)))
@@ -82,32 +82,30 @@ def find_intersection_dots_right(a: float, b: float, c: float, d: float) -> _Tup
         if type(i) not in (int, float):
             raise TypeError(f'Сoefficient expected to have type - float, got - {type(i)}')
 
-    x, y = _sympy.symbols('x, y')
-    left = _sympy.Eq(a * x ** 2 + b * x + c, 0)
-    right1 = _sympy.Eq(x - 1 - d, 0)
-    right2 = _sympy.Eq(-x + 1 - d, 0)
-    right3 = _sympy.Eq(-x + 1 + d, 0)
-    right4 = _sympy.Eq(x - 1 + d, 0)
+    def _y(_x: float) -> float:
+        return abs(abs(_x - 1) - d)
 
-    solution1 = _sympy.solve((left, right1), (x, y))
-    solution2 = _sympy.solve((left, right2), (x, y))
-    solution3 = _sympy.solve((left, right3), (x, y))
-    solution4 = _sympy.solve((left, right4), (x, y))
+    x = _sympy.Symbol('x')
 
-    result1 = [(_x.evalf(), _y.evalf()) for (_x, _y) in solution1
-               if type(_x.evalf()) != _sympy.core.add.Add and type(_y.evalf()) != _sympy.core.add.Add]
+    solution1 = _sympy.solve(a * x ** 2 + b * x + c - (x - 1 - d), x)
+    solution2 = _sympy.solve(a * x ** 2 + b * x + c - (-x + 1 - d), x)
+    solution3 = _sympy.solve(a * x ** 2 + b * x + c - (-x + 1 + d), x)
+    solution4 = _sympy.solve(a * x ** 2 + b * x + c - (x - 1 + d), x)
+
+    result1 = [(_x.evalf(), _y(_x.evalf())) for _x in solution1 if type(_x.evalf()) not in (_sympy.core.add.Add,
+                                                                                            _sympy.core.mul.Mul)]
     result1 = [(x, y) for (x, y) in result1 if x >= 1 and abs(x - 1) - d >= 0]
 
-    result2 = [(_x.evalf(), _y.evalf()) for (_x, _y) in solution2
-               if type(_x.evalf()) != _sympy.core.add.Add and type(_y.evalf()) != _sympy.core.add.Add]
+    result2 = [(_x.evalf(), _y(_x.evalf())) for _x in solution2 if type(_x.evalf()) not in (_sympy.core.add.Add,
+                                                                                            _sympy.core.mul.Mul)]
     result2 = [(x, y) for (x, y) in result2 if x < 1 and abs(x - 1) - d >= 0]
 
-    result3 = [(_x.evalf(), _y.evalf()) for (_x, _y) in solution3
-               if type(_x.evalf()) != _sympy.core.add.Add and type(_y.evalf()) != _sympy.core.add.Add]
+    result3 = [(_x.evalf(), _y(_x.evalf())) for _x in solution3 if type(_x.evalf()) not in (_sympy.core.add.Add,
+                                                                                            _sympy.core.mul.Mul)]
     result3 = [(x, y) for (x, y) in result3 if x >= 1 and abs(x - 1) - d < 0]
 
-    result4 = [(_x.evalf(), _y.evalf()) for (_x, _y) in solution4
-               if type(_x.evalf()) != _sympy.core.add.Add and type(_y.evalf()) != _sympy.core.add.Add]
+    result4 = [(_x.evalf(), _y(_x.evalf())) for _x in solution4 if type(_x.evalf()) not in (_sympy.core.add.Add,
+                                                                                            _sympy.core.mul.Mul)]
     result4 = [(x, y) for (x, y) in result4 if x < 1 and abs(x - 1) - d < 0]
 
     result = result1 + result2 + result3 + result4
